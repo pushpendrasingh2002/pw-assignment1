045bb46259d373fdfd58ebf410a2b5a4c9d0464b
pushpendrasingh2002 <152796363+pushpendrasingh2002@users.noreply.github.com>
2024-02-17 22:07:19 +0530

    upload

---
+++ assignment2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int num1=5;
+int num2=2;
+
+cout<<num1*num2<<endl;
+
+
+
+
+
+}
---
+++ assingment5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+float radius=10;
+float pi= 3.1415;
+float circumference ;
+circumference  = 2*pi*radius;
+
+cout<<circumference<<endl;
+
+return 0;
+
+}
+
+
+
+
+
+
